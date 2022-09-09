424b80d400487014d04ab71e3be8f0c52bc34a9c
Muhtasim Munem Mugdho <muhtasim.mugdho@bjitgroup.com>
2022-09-09 08:50:44 +0600

    feat(redux): redux structure implemented

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,7 +2,7 @@ import { createStore } from 'redux';
 
 const initialState = { counter: 0, showCounter: true };
 
-const counterReducer = (state = initialState, action:object) => {
+const counterReducer = (state = initialState, action:any) => {
   if (action.type === 'increment') {
     return {
       counter: state.counter + 1,
